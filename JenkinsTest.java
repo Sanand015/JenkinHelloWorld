49c0743ab96000d1ac3ce5ee744b478432de9c96
Shubham Anand <shubhamanandzeroplus@gmail.com>
2021-12-27 22:27:53 +0530

    Adding new files'

---
+++ JenkinsTest.java
@@ -0,0 +1,14 @@
+package TestingForJenkins;
+
+import java.util.Scanner;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in); 
+		System.out.println("Hello Jenkins");
+
+	}
+
+}
